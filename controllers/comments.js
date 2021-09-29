46869f89006d204de5df4964d291dc6adedbe6aa
PratikAwaik <pratikawaik125@gmail.com>
2021-09-30 00:29:10 +0530

    controllers for upvotes and downvotes

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,32 +3,92 @@ const Comment = require("../models/comment");
 const Post = require("../models/post");
 
 const getAllComments = async (req, res) => {
-  const id = req.params.id;
-  const comments = await Comment.find({ post: id })
+  const postId = req.params.id;
+  const comments = await Comment.find({ post: postId })
     .populate("user")
     .populate("repliedTo");
   res.json(comments);
 };
 
 const createComment = async (req, res) => {
-  const id = mongoose.Types.ObjectId(req.params.id);
+  const postId = req.params.id;
   const body = req.body;
   const newComment = new Comment({
     content: body.content,
-    post: id,
+    post: mongoose.Types.ObjectId(postId),
     user: req.user.id,
     repliedTo: body.commentId ? body.commentId : null,
     createdAt: Date.now(),
   });
   await newComment.save();
+  const populatedComment = await newComment.populate("user");
 
-  const post = await Post.findById(id);
+  const post = await Post.findById(postId);
   post.comments = post.comments.concat(newComment);
   post.save();
-  res.json(newComment);
+
+  req.user.comments = req.user.comments.concat(newComment.id);
+  req.user.save();
+  res.json(populatedComment);
+};
+
+const upvoteComment = async (req, res) => {
+  const body = req.body;
+  const commentId = req.params.commentId;
+
+  if (body.hasUpvotedAlready) {
+    req.user.commentsUpvoted = req.user.commentsUpvoted.filter(
+      (id) => id.toString() !== commentId
+    );
+  } else if (body.hasDownvotedAlready) {
+    req.user.commentsUpvoted = req.user.commentsUpvoted.concat(commentId);
+    req.user.commentsDownvoted = req.user.commentsDownvoted.filter(
+      (id) => id.toString() !== commentId
+    );
+  } else {
+    req.user.commentsUpvoted = req.user.commentsUpvoted.concat(commentId);
+  }
+  await req.user.save();
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    { upvotes: body.upvotes, downvotes: body.downvotes },
+    { new: true }
+  ).populate("user");
+
+  res.json(updatedComment);
+};
+
+const downvoteComment = async (req, res) => {
+  const body = req.body;
+  const commentId = req.params.commentId;
+
+  if (body.hasDownvotedAlready) {
+    req.user.commentsDownvoted = req.user.commentsDownvoted.filter(
+      (id) => id.toString() !== commentId
+    );
+  } else if (body.hasUpvotedAlready) {
+    req.user.commentsDownvoted = req.user.commentsDownvoted.concat(commentId);
+    req.user.commentsUpvoted = req.user.commentsUpvoted.filter(
+      (id) => id.toString() !== commentId
+    );
+  } else {
+    req.user.commentsDownvoted = req.user.commentsDownvoted.concat(commentId);
+  }
+  await req.user.save();
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    commentId,
+    { upvotes: body.upvotes, downvotes: body.downvotes },
+    { new: true }
+  ).populate("user");
+
+  res.json(updatedComment);
 };
 
 module.exports = {
   getAllComments,
   createComment,
+  upvoteComment,
+  downvoteComment,
 };
