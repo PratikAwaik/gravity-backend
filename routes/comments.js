219f332705b764c1c1a84d23c9af9c796963b58e
PratikAwaik <pratikawaik125@gmail.com>
2021-09-30 00:28:42 +0530

    add routes for comment upvotes and downvotes

--- routes/comments.js
+++ routes/comments.js
@@ -10,4 +10,10 @@ router.get("/", commentsController.getAllComments);
 // post a comment
 router.post("/", middleware.userExtractor, commentsController.createComment);
 
+// upvote comment
+router.patch("/:commentId/upvote", middleware.userExtractor, commentsController.upvoteComment);
+
+// downvote comment
+router.patch("/:commentId/downvote", middleware.userExtractor, commentsController.downvoteComment);
+
 module.exports = router;
