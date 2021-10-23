9b18852e293d9a6f0dd5ceae470bde3c9dc97401
PratikAwaik <pratikawaik125@gmail.com>
2021-10-24 01:19:15 +0530

    add random color to subreddit's cover color

--- controllers/subreddit.js
+++ controllers/subreddit.js
@@ -10,6 +10,15 @@ const getSingleSubreddit = async (req, res) => {
   res.json(subreddit);
 };
 
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 const createSubreddit = async (req, res) => {
   const body = req.body;
   const newSubreddit = new Subreddit({
@@ -18,6 +27,7 @@ const createSubreddit = async (req, res) => {
     communityIcon: body.image,
     members: [req.user.id],
     moderators: [req.user.id],
+    coverColor: getRandomColor(),
   });
 
   await newSubreddit.save();
